e4a220db5a10f564a28dd5be5ca74ec454d9d583
Mrina <mrina@Mrinalinis-MacBook-Pro.local>
2017-09-01 14:13:28 -0700

    finished pt2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -33,15 +33,15 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+ s =~ /^[^aeiou\W]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  ((!s.empty?) && (s.to_i(2) % 4 == 0) && (s.chars.all? {|c| c=~/[01]/}))
 end
 
 # Part 3
